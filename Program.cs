d12dc8dc2269cf051f62f83e2ee88a74b017a904
RangsanLaravel <dethman_light@hotmail.com>
2020-03-09 22:56:06 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace coreOrmSqlite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new DatabaseContext())
+            {
+    
+            var product = new Product(){ Price = 100, Description = "Movie" };
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            foreach(var p in db.Products) 
+            {
+                Console.WriteLine("{0} {1} {2}", p.ProductId, p.Description, p.Price);
+            }
+    
+            }
+        }
+    }
+}
